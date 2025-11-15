f80761abab16f9231ea8cba4cc2308d0f70f33dc
Diksha Pal <dpal@INSPIRON14.localdomain>
2025-11-15 17:13:17 -0500

    tts service up sst skeleton up eleven API integration done

---
+++ backend/src/tts_service.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
+from elevenlabs.play import play
+import os
+
+def text_to_speech(text: str) -> bytes:
+    elevenlabs = ElevenLabs(api_key=os.getenv("ELEVENLABS_API_KEY"))
+    audio = elevenlabs.text_to_speech.convert(
+        text=text,
+        voice_id="JBFqnCBsd6RMkjVDRZzb",
+        model_id="eleven_multilingual_v2",
+        output_format="mp3_44100_128",
+    )
+    return audio 
